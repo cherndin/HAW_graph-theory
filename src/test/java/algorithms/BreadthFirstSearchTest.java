2da6fa7aedd39073640ce704c6afce53b2bb332e
Neak <legendmax@live.de>
2016-10-23 18:52:18 +0200

    Created Testcase for Breadth First Search

---
+++ src/test/java/algorithms/BreadthFirstSearchTest.java
@@ -0,0 +1,22 @@
+package algorithms;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Neak on 23.10.2016.
+ */
+public class BreadthFirstSearchTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void compute() throws Exception {
+
+    }
+
+}
