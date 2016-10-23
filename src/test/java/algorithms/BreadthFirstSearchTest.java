e442906a29b5f0ab2cc8bf5642bac1f86960614c
Neak <legendmax@live.de>
2016-10-23 19:38:32 +0200

    small testcase (not finished) for bfs

--- src/test/java/algorithms/BreadthFirstSearchTest.java
+++ src/test/java/algorithms/BreadthFirstSearchTest.java
@@ -1,5 +1,7 @@
 package algorithms;
 
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,7 +18,21 @@ public class BreadthFirstSearchTest {
 
     @Test
     public void compute() throws Exception {
+        Graph testGraph = new SingleGraph("testSave");
+        testGraph.addNode("1");
+        testGraph.addNode("2");
+        testGraph.addNode("3");
+        testGraph.addNode("4");
 
-    }
+        testGraph.addEdge("12", "1", "2");
+        testGraph.addEdge("23", "2", "3");
+        testGraph.addEdge("34", "3", "4");
+        testGraph.display();
+
+
+        BreadthFirstSearch.init(testGraph);
+
+
+        assertEquals({3,3}, shortTraverse(testGraph)); // {shortestWay, anzKanten}
 
 }
