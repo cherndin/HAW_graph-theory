bc5da185e964d66736e80643451cc3695980a7d8
MattX7 <lion.pierau@gmail.com>
2016-11-25 10:58:16 +0100

    VS test added

---
+++ src/test/java/algorithms/river_issue/EdmondsKarpVSFordFulkersonTest.java
@@ -0,0 +1,18 @@
+package algorithms.river_issue;
+
+import org.junit.Before;
+
+/**
+ * Created by MattX7 on 25.11.2016.
+ * 1.) Testen Sie f¨ur Graph4 dabei Ford und Fulkerson sowie Edmonds und Karp und geben Sie den maximalen Fluss, sowie die Laufzeit der beiden Algorithmen an.
+ * 2.) Implementierung eines Netzwerks (gerichteter, gewichteter Graph mit Quelle und Senke) BigNet Gruppe TeamNr mit 50 Knoten und etwa 800 Kanten. Beschreiben Sie die Konstruktion von BigNet Gruppe TeamNr
+ * und erl¨autern sie, inwiefern die Konstruktion ein Netzwerk liefert.
+ * 3.) Lassen Sie bitte beide Algorithmen auf dem Netzwerk BigNet Gruppe TeamNr, den maximalen Fluss berechnen und vergleichen diese.
+ * 4.) Benutzen Sie bitte unterschiedlich große Big-Net-Graphen mit 800 Knoten und 300.000 Kanten und mit 2.500 Knoten und 2.000.000 Kanten. Und lassen Sie bitte Ihre die Algorithmen jeweils 100 durchlaufen.
+ */
+public class EdmondsKarpVSFordFulkersonTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
